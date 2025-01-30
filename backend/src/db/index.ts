660891bf8cac84fe39f486827979eeb43b106a5e
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-01-31 01:38:49 +0530

    database connection

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
 
-async function connectDB(){
-    try {
-        const connectionInstance = 
-        
-    } catch (error) {
-        
-    }
+async function connectDB() {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `{process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MONGODB Connection Successful, HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(`MONGODB Connection failed ERROR:${error}`);
+    process.exit(1);
+  }
 }
+
+export default connectDB;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,18 @@
-import express from "express"
+import express from "express";
+import connectDB from "./db";
+import { app } from "./app";
 
-connectDB
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is serving on PORT: ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(`MONGODB Connection failed!`, error);
+  });
