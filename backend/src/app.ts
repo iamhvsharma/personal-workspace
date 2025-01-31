2a0be42760246d95a342c12b03785046edbfd6d8
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-01-31 10:41:26 +0530

    route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,13 @@
 import express, { urlencoded } from "express";
+import authRoutes from "./routes/auth.route"
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/v1/auth", authRoutes)
 
 export { app };
