9da9b89176cc729acd70bd9e3d7cc2bbb84abd4e
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-06-11 16:50:11 +0530

    Update package.json script name and add content routes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
 import authRoutes from "./routes/auth.route"
+import contentRoutes from "./routes/content.route"
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/content", contentRoutes)
 
 export { app };
